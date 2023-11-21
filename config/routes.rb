e9c4c000acd40d78643b9248599f337b5a5c5d71
Fatima Najafi <najafifatima762@gmail.com>
2023-11-21 09:34:58 +0430

    Config the devise dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # get 'exchanges/index'
+  # get 'exchanges/new'
+  # get 'categories/index'
+  # get 'categories/new'
+  # root 'splash_screen/index'
+  #   authenticated :user do
+  #   root "categories#index", as: :authenticated_root
+  # end
+
+  # unauthenticated do
+  #   root "splash_screen#index", as: :unauthenticated_root
+  # end
+   
+
+  resources :categories do
+    resources :exchanges
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +24,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   root "categories#index"
 end
