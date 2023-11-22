136c49c94b0e9c7d970bca92d35047b05f2b4b2e
Fatima Najafi <najafifatima762@gmail.com>
2023-11-22 18:25:26 +0430

    Add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,17 @@ Rails.application.routes.draw do
   # get 'categories/index'
   # get 'categories/new'
   # root 'splash_screen/index'
-  #   authenticated :user do
-  #   root "categories#index", as: :authenticated_root
-  # end
+    authenticated :user do
+    root "categories#index", as: :authenticated_root
+  end
 
-  # unauthenticated do
-  #   root "splash_screen#index", as: :unauthenticated_root
-  # end
+  unauthenticated do
+    root "splash_screen#index", as: :unauthenticated_root
+  end
    
 
   resources :categories do
     resources :exchanges
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-   root "categories#index"
 end
