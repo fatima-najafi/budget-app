49da58c893fbce8254ffdda4461197b6ec5e903c
Fatima Najafi <najafifatima762@gmail.com>
2023-11-23 16:01:24 +0430

    Add the edit and delete actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,6 +17,27 @@ class CategoriesController < ApplicationController
     end
   end
 
+   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Category was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+def destroy
+  @category = Category.find(params[:id])
+  @category.exchanges.destroy_all
+  @category.destroy
+  redirect_to categories_path, notice: 'Category was successfully destroyed.'
+end
+
   private
 
   def category_params
