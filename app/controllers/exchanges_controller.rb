34dd097e99620b0111a5c87dbe256f567f6c8bb1
Fatima Najafi <najafifatima762@gmail.com>
2023-11-23 16:01:43 +0430

    Add the edit and delete actions for exhange

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -19,6 +19,29 @@ class ExchangesController < ApplicationController
     end
   end
 
+  
+ def edit
+  @category = Category.find(params[:category_id])
+  @transaction = Exchange.find(params[:id])
+end
+
+  def update
+    @transaction = Exchange.find(params[:id])
+    if @transaction.update(exchange_params)
+      redirect_to category_exchanges_path(exchange_params[:category_id]),
+                  notice: 'Transaction was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @transaction = Exchange.find(params[:id])
+    @transaction.destroy
+    redirect_to category_exchanges_path(params[:category_id]),
+                notice: 'Transaction was successfully destroyed.'
+  end
+
   def exchange_params
     params.require(:exchange).permit(:name, :amount, :category_id)
   end
