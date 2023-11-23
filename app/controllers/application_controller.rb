1b28ff35b2bdc006c3d74b7539f6ed20bbb82a58
Fatima Najafi <najafifatima762@gmail.com>
2023-11-23 18:48:10 +0430

    Remove unneccessary code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::Base
-  #  helper_method :current_user
   protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
   end
-
-
-  #   def current_user
-  #     @current_user ||= User.find_by(id: session[:user_id])
-  #   end
 end
